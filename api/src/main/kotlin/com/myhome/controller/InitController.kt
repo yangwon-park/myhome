4349dbc8938687ca47b51b73e5d9ee7fda253173
ywonp94 <ywonp94@gmail.com>
2025-01-08 18:39:13 +0900

    :necktie: "Dwelling 엔티티 생성"

---
+++ api/src/main/kotlin/com/myhome/controller/InitController.kt
@@ -0,0 +1,14 @@
+package com.myhome.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class InitController {
+	@GetMapping
+	fun init(): String {
+		return "ok"
+	}
+}
---
+++ domain/src/main/kotlin/com/myhome/Dwelling.kt
@@ -0,0 +1,12 @@
+package com.myhome
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "dwelling")
+class Dwelling(
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "dwelling_id")
+	val id: Long? = null,
+)
