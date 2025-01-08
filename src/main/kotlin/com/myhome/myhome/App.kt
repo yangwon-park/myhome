ab83cdd614890511eb144f9b4da4bf6081ba3e9f
ywonp94 <ywonp94@gmail.com>
2025-01-08 16:11:13 +0900

    init

---
+++ src/main/kotlin/com/myhome/myhome/App.kt
@@ -0,0 +1,11 @@
+package com.myhome.myhome
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App
+
+fun main(args: Array<String>) {
+	runApplication<App>(*args)
+}
